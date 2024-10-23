e4477005e37b23f5904ea9eb4437daaae19bcacc
Dinkal-Kakadiya <dinkal.kakadiya@ecelanceit.in>
2024-10-23 09:46:03 +0530

    Initial Project With root navigation

---
+++ src/navigations/RootNavigation.js
@@ -0,0 +1,20 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Home from '../screens/Home';
+
+const RootNavigation = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigation;
+
+const styles = StyleSheet.create({});
